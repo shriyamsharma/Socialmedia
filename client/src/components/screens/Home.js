6f98996377adac077933308dfddcbcac77e204e3
shriyamsharma <sharmashriyam676@gmail.com>
2020-12-04 20:39:43 +0530

    bug fixed v0.1.1

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -119,11 +119,18 @@ const Home = () => {
                 data.map(item => {
                     return(
                         <div className="card home-card" key={item._id}>
-                            <h5 style={{padding:"4px", marginLeft:"4px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id:"/profile"}>{item.postedBy.name}</Link> {item.postedBy._id == state._id && 
+                            {/* <h5 style={{padding:"4px", marginLeft:"4px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id:"/profile"}>{item.postedBy.name}</Link> {item.postedBy._id == state._id && 
                                 <i className="small material-icons" 
                                 style={{float:"right"}}
                                 onClick={() => deletePost(item._id)}
-                                >delete_forever</i>} </h5>
+                                >delete_forever</i>} </h5> */}
+                            <h5 style={{padding:"4px"}}><Link to={item.postedBy._id !== state._id?"/profile/"+item.postedBy._id :"/profile"  }>{item.postedBy.name}</Link> {item.postedBy._id == state._id 
+                            && <i className="material-icons" style={{
+                                float:"right"
+                            }} 
+                            onClick={()=>deletePost(item._id)}
+                            >delete_forever</i>
+                            }</h5>
                             <div className="card-image">
                                 <img src={item.photo}  alt=""/> 
                             </div>
